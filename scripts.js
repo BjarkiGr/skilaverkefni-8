6f3389046dc97b6ce4134b9fd67cfd5fa7b0d4ef
BjarkiGr <bjg56@hi.is>
2019-11-05 11:53:04 +0000

    kláraði function finish

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const ENTER_KEYCODE = 13;
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.form');
-  const items = document.querySelector('.items');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form");
+  const items = document.querySelector(".items");
 
   text.init(form, items);
 });
@@ -12,19 +12,27 @@ const text = (() => {
 
   function init(_form, _items) {
     items = _items;
-    _form.addEventListener('submit', formHandler);
-
+    _form.addEventListener("submit", formHandler);
+    for (let item of items.querySelectorAll(".item")) {
+      const checkbox = item.querySelector("item__checkbox");
+      checkbox.addEventListener("click", finish);
+      const button = item.querySelector(".item__button");
+      button.addEventListener("click", deleteItem);
+      const text = item.querySelector("item__text");
+      text.addEventListener("click", edit);
+    }
     // TODO láta hluti í _items virka
   }
 
   function formHandler(e) {
     e.preventDefault();
-
-    console.log('halló heimur');
+    console.log("Halló heimur");
+    const input = e.target.querySelector('.form__input');
   }
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+    e.target.parentNode.classList.toggle('item--done');
   }
 
   // event handler fyrir það að breyta færslu
@@ -36,18 +44,15 @@ const text = (() => {
   }
 
   // fall sem sér um að bæta við nýju item
-  function add(value) {
-  }
+  function add(value) {}
 
   // event handler til að eyða færslu
-  function deleteItem(e) {
-  }
+  function deleteItem(e) {}
 
   // hjálparfall til að útbúa element
-  function el(type, className, clickHandler) {
-  }
+  function el(type, className, clickHandler) {}
 
   return {
     init: init
-  }
+  };
 })();
